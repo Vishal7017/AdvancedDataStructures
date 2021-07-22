652a41585cedbd36ef1ad151b614d862ff3f2b1c
Dashq <Newtab@gmail.com>
2021-07-22 09:38:21 +0530

    RecusrsionMultiplication

---
+++ RecusrionMultiplication.py
@@ -0,0 +1,11 @@
+def multiplication(num_a, num_b):
+  if num_a == 0 or num_b == 0:
+    return 0
+
+  return num_a + multiplication(num_a, num_b - 1)
+
+
+# test cases
+print(multiplication(3, 7) == 21)
+print(multiplication(5, 5) == 25)
+print(multiplication(0, 4) == 0)
