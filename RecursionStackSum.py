272636d6f5591f3dd3be44218310f7b98ee16116
Dashq <Newtab@gmail.com>
2021-08-16 15:03:35 +0530

    Recursion

---
+++ RecursionStackSum.py
@@ -0,0 +1,13 @@
+def sum_to_one(n):
+  result = 1
+  call_stack = []
+  while (n >= 1):
+    execution_context = {"n_value": n}
+    call_stack.append(execution_context)
+    n -= 1
+    print(call_stack)
+  print("BASE CASE REACHED")
+  return result, call_stack
+
+
+print(sum_to_one(12))
