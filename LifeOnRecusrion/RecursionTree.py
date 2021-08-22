7a28ddeb32a931d5f24b018b6759d6eee1fafaeb
Dashq <Newtab@gmail.com>
2021-08-22 19:56:01 +0530

    TreeBinary

--- LifeOnRecusrion/RecursionTree.py
+++ LifeOnRecusrion/RecursionTree.py
@@ -1,8 +1,15 @@
- # Define build_bst() below...
+# Define build_bst() below...
 def build_bst(my_list):
   if len(my_list) == 0:
     return "No Child"
-  middle_idx = my_list(len(my_list)/2)
+  middle_idx = len(my_list)//2
+  middle_value = my_list[middle_idx]
+  print("Middle index: {}".format(str(middle_idx)))
+  print("Middle value: {}".format(str(middle_value)))
+  tree_node = {"data": middle_value}
+  tree_node["left_child"] = build_bst(my_list[:middle_idx])
+  tree_node["right_child"] = build_bst(my_list[middle_idx+1 : ])
+  return tree_node
   
 
 # For testing
@@ -12,4 +19,4 @@ print(binary_search_tree)
 
 # fill in the runtime as a string
 # 1, logN, N, N*logN, N^2, 2^N, N!
-runtime = "???"
+runtime = "N*logN"
