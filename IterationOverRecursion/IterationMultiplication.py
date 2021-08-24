b3a4e7c7e5c33dc4b5c2fb9047ab8f0cdba602ad
Vishal7017 <wavhalev@gmail.com>
2021-08-24 12:27:10 +0530

    RecursionAndIteration

---
+++ IterationOverRecursion/IterationMultiplication.py
@@ -0,0 +1,9 @@
+def multiplication(num1, num2):
+    result = 0
+    for count in range(0, num2):
+        result += num1
+    return result
+
+print(multiplication(4,5))
+print(multiplication(12,23))
+print(multiplication(15,2))
