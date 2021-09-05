03848796102fa917cfe7a720bf4639694fa0c0ef
Vishal7017 <wavhalev@gmail.com>
2021-09-05 22:39:28 +0530

    LinkedListAsymNotation

---
+++ LinkedInAList/MaxValueInLinkedList.py
@@ -0,0 +1,83 @@
+class Node:
+    def __init__(self, value, next_node=None):
+        self.value = value
+        self.next_node = next_node
+    
+    def set_next_node(self, next_node):
+        self.next_node = next_node
+    
+    def get_next_node(self):
+        return self.next_node
+
+    def get_value(self):
+        return self.value
+
+
+class LinkedList:
+    def __init__(self, value=None):
+        self.head_node = Node(value)
+
+    def get_head_node(self):
+        return self.head_node
+    
+    def insert_beginning(self, new_value):
+        new_node = Node(new_value)
+        new_node.set_next_node(self.head_node)
+        self.head_node = new_node
+    
+    def stringify_list(self):
+        string_list = ""
+        current_node = self.head_node
+        while current_node:
+            string_list += str(current_node.value) + "\n"
+            current_node = current_node.get_next_node()
+        return string_list
+
+    def remove_node(self, value_to_remove):
+        current_node = self.head_node
+        if current_node.get_value() == node_to_remove:
+            self.head_node = current_node.get_next_node()
+        else:
+            while current_node:
+                next_node = current_node.get_next_node()
+                if next_node.get_value() == value_to_remove:
+                    current_node.next_node = next_node.get_next_node()
+                    current_node = None
+                else:
+                    current_node = next_node
+
+def find_max(linked_list):
+    print("----------------------")
+    print("Finding the maximum value of:\n{0}".format(linked_list.stringify_list()))
+    current = linked_list.get_head_node()
+    maximum = current.get_value()
+    while current.get_next_node():
+        current = current.get_next_node()
+        val = current.get_value()
+        if val > maximum:
+            maximum = val
+    return maximum
+
+ll = LinkedList(6)
+ll.insert_beginning(32)
+ll.insert_beginning(-12)
+ll.insert_beginning(48)
+ll.insert_beginning(2)
+ll.insert_beginning(1)
+
+print("The maximum value in this linked list is {0}\n".format(find_max(ll)))
+
+ll_2 = LinkedList(60)
+ll_2.insert_beginning(12)
+ll_2.insert_beginning(22)
+ll_2.insert_beginning(-10)
+print("The maximum value in this linked list is {0}\n".format(find_max(ll_2)))
+
+
+ll_3 = LinkedList("A")
+ll_3.insert_beginning("X")
+ll_3.insert_beginning("V")
+ll_3.insert_beginning("L")
+ll_3.insert_beginning("D")
+ll_3.insert_beginning("Q")
+print("The maximum value in this linked list is {0}".format(find_max(ll_3)))
