e5f54aef1670a70084fc819c59b30454ad6015e0
Vishal7017 <wavhalev@gmail.com>
2021-11-22 20:39:37 +0530

    HashMap

--- StoredInHashMap/FlowerBlossomHashMap/script.py
+++ StoredInHashMap/FlowerBlossomHashMap/script.py
@@ -2,6 +2,56 @@ from linked_list import Node, LinkedList
 from blossom_lib import flower_definitions
 
 class HashMap:
+  
+  def __init__(self, size):
+    self.array_size = size
+    self.array = [LinkedList() for number in range(size)]
+
+  def hash(self, key):
+    return sum(key.encode())
+    
+  def compress(self, hash_code):
+    return hash_code % self.array_size
+
+  def assign(self, key, value):
+    array_index = self.compress(self.hash(key))
+    payload = Node([key, value])
+    #self.array[array_index] = [key, value]
+    list_at_array = self.array[array_index]
+    for item in list_at_array:
+      if item[0] is key:
+        item[1] = value
+        return
+    list_at_array.insert(payload)
+
+  def retrieve(self, key):
+    array_index = self.compress(self.hash(key))
+    list_at_index = self.array[array_index]
+    for item in list_at_index:
+      if item[0] is key:
+        return item[1]
+    return None
+
+blossom = HashMap(len(flower_definitions))
+for flower in flower_definitions:
+  blossom.assign(flower[0], flower[1])
+
+print(blossom.retrieve('daisy'))
+print(blossom.retrieve('rose'))
+print(blossom.retrieve('sunflower'))
+print(blossom.retrieve('wisteria'))
+print(blossom.retrieve('morning glory'))
+print(blossom.retrieve('periwinkle'))
+
+
+
+
+
+"""
+from linked_list import Node, LinkedList
+from blossom_lib import flower_definitions
+
+class HashMap:
   def __init__(self, size):
     self.array_size = size
     self.array = [LinkedList() for i in range(size)]
@@ -41,3 +91,5 @@ for flower in flower_definitions:
   
 
 print(blossom.retrieve('rose'))
+
+"""
