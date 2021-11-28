2f137b80ab27269a233da0af9e0391bf917e6ae1
Vishal7017 <wavhalev@gmail.com>
2021-11-28 23:49:57 +0530

    Rcursion

--- LifeOnRecusrion/RecursionBaseCase.py
+++ LifeOnRecusrion/RecursionBaseCase.py
@@ -2,11 +2,14 @@
 def sum_to_one(n):
   result = 1
   call_stack = []
-  while n != 1:
+  while n is not 1:
     execution_context = {"n_value": n}
     call_stack.append(execution_context)
     n -= 1
     print(call_stack)
   print("BASE CASE REACHED")
   return result, call_stack
+
+
+
 sum_to_one(4)
