37e8bb690df51c3be12e0733f231a722da2f824d
Vishal7017 <wavhalev@gmail.com>
2021-11-28 23:56:20 +0530

    Recu

--- LifeOnRecusrion/RecursionBaseCase.py
+++ LifeOnRecusrion/RecursionBaseCase.py
@@ -1,15 +1,23 @@
-# define your sum_to_one() function above the function call
 def sum_to_one(n):
   result = 1
   call_stack = []
-  while n is not 1:
+  
+  while n != 1:
     execution_context = {"n_value": n}
     call_stack.append(execution_context)
     n -= 1
     print(call_stack)
   print("BASE CASE REACHED")
+
+  while len(call_stack) > 0:
+    return_value= call_stack.pop()
+    print(call_stack)
+    print("return_value[""] to result")
+    result = result+return_value["n_value"]
+
+    
+
+
   return result, call_stack
-
-
 
 sum_to_one(4)
