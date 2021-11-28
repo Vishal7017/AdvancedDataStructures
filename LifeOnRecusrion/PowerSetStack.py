e1fd246e6b31fa4d00837918c2e622d07ed9c13b
Vishal7017 <wavhalev@gmail.com>
2021-11-29 00:47:23 +0530

    Powerset

--- LifeOnRecusrion/PowerSetStack.py
+++ LifeOnRecusrion/PowerSetStack.py
@@ -14,3 +14,18 @@ power_set_of_universities = power_set(universities)
 
 for set in power_set_of_universities:
   print(set)
+
+
+def power_set2(set):
+    power_set_size = 2**len(set)
+    result = []
+
+    for bit in range(0, power_set_size):
+        sub_set = []
+        for binary_digit in range(0, len(set)):
+            if((bit & (1 << binary_digit)) > 0):
+                sub_set.append(set[binary_digit])
+        result.append(sub_set)
+    return result
+
+print(power_set2(["a","b","c","d","e"]))
