990c68cbdcad6fbc83b6d40e9f4325a534d1a746
Vishal7017 <wavhalev@gmail.com>
2021-12-01 22:30:59 +0530

    Fib

--- IterationOverRecursion/FibonacciSeries.py
+++ IterationOverRecursion/FibonacciSeries.py
@@ -13,5 +13,18 @@ def fibonacci(n):
 	
 	
 	
+
+def fibonacci(n):
+	if n <= 0:
+		ValueError("Enter number greater than zero")
+
+	fibs = [0, 1]
+	if n <= len(fibs) - 1:
+		return fibs[n]
+
+	while n > len(fibs) -1 :
+		fibs.append(fibs[-1] + fibs[-2])
+
+	return fibs[-1]	
 	
 	
