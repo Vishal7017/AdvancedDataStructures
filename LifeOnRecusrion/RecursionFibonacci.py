2b141c32e4de1c8411f54f4262d3459f06455c47
Vishal7017 <wavhalev@gmail.com>
2021-12-01 22:23:03 +0530

    Fibonacci

--- LifeOnRecusrion/RecursionFibonacci.py
+++ LifeOnRecusrion/RecursionFibonacci.py
@@ -12,3 +12,19 @@ fibonacci(5)
 
 # 1, logN, N, N^2, 2^N, N!
 fibonacci_runtime = "2^N"
+
+
+# define the fibonacci() function below...
+def fibonacci(n):
+  if n == 1:
+    return 1
+  if n == 0:
+    return 0
+  return fibonacci(n-1)+fibonacci(n-2)
+
+
+
+print(fibonacci(9))
+# set the appropriate runtime:
+# 1, logN, N, N^2, 2^N, N!
+fibonacci_runtime = "2^N"
