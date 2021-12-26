166bb33dd9643ff798a74893fb583117c9b66bd3
Vishal7017 <wavhalev@gmail.com>
2021-12-26 11:18:59 +0530

    Tests

--- Functions/FibonacciDictionary.py
+++ Functions/FibonacciDictionary.py
@@ -9,3 +9,16 @@ def fib_efficient(n, d):
 d = {1:1, 2:2}
 
 print(fib_efficient(6, d))
+
+def fib_eff(n,d):
+    if n in d:
+        return d[n]
+    else:
+        ans = fib_eff(n-1, d) + fib_eff(n-2, d)
+        d[n] = ans
+        return ans
+    
+
+
+m = {1:1, 2:2}
+print(fib_eff(6,m))
---
+++ Functions/TestingandDebug/RaisinganException.py
@@ -0,0 +1,21 @@
+def get_ratios(L1, L2):
+    """Assume: L1 and L2 are lists of equal lenght of numbers
+       Returns: a list containinng L1[i]/L2[i]"""
+    ratios = []
+    for index in range(len(L1)):
+        try: 
+            ratios.append(L1[index]/L2[index])
+        except ZeroDivisionError:
+            ratios.append(float('nan')) # nan-not a number
+        except:
+            raise ValueError('get_ratios called with bad arg')
+    return ratios
+
+L1 = [4, 16, 32, 64, 128]
+L2 = [3, 0, 12, 32, 12]
+print(get_ratios(L1, L2))
+
+
+# L3 = [4, 16, 32, 64, 128]
+# L4 = [3, 0, 12, "3", 12]
+# print(get_ratios(L3, L4))
