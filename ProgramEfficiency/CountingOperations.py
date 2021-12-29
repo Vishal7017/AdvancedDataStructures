d616dc6c2e6beed1b1cf67333a1b3620bede5b73
Vishal7017 <wavhalev@gmail.com>
2021-12-29 12:23:22 +0530

    Effeciency

---
+++ ProgramEfficiency/CountingOperations.py
@@ -0,0 +1,8 @@
+def c_to_f(c):
+    return c*9.0/5 + 32
+
+def mysum(x):
+    total = 0
+    for i in range(x+1):
+        total += i
+    return total
---
+++ ProgramEfficiency/DifferentInputs.py
@@ -0,0 +1,7 @@
+# a function that searches for an element inn a list
+
+def search_for_elmt(L, e):
+    for i in L:
+        if i == e:
+            return True
+    return True
