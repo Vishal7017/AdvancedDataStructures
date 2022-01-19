9ebd0cd38fa602fe75b2e2bcb00c2b041703f219
Vishal7017 <wavhalev@gmail.com>
2022-01-19 15:49:13 +0530

    SelectionSortRec

--- Sorting/SelectionSort.py
+++ Sorting/SelectionSort.py
@@ -9,9 +9,21 @@ def selection_sort(L):
 
 testList = [1,3,4,5,6,743,23,54,535,2355]
 
-print('')
-print(selection_sort(testList))
-print(testList)
+# print('')
+# print(selection_sort(testList))
+# print(testList)
+
+
+
+#Selection Sort Recursive
+
+def selection_sort2(A, i = None):
+    '''Sort A[:i + 1]'''
+    if i is None: i = len(A) - 1
+    if i > 0:
+        j = prefix_max(A, i)
+        A[i], A[j] = A[j], A[i]
+        selection_sort2(A, i - 1)
 
 #Helper Function for selection sort
 def prefix_max(A, i):
@@ -21,3 +33,7 @@ def prefix_max(A, i):
         if A[i] < A[j]:
             return j
     return i
+
+print('')
+print(selection_sort2(testList))
+print(testList)
