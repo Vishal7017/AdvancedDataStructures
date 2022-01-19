9e4db6297db7d576aec145af3cfaba8760fcde37
Vishal7017 <wavhalev@gmail.com>
2022-01-19 18:03:05 +0530

    Merge Sorties

--- Sorting/MergeSort.py
+++ Sorting/MergeSort.py
@@ -35,3 +35,25 @@ testList = [1,3,4,5,6,743,23,54,535,2355]
 print('')
 print(merge_sort(testList))
 
+
+def merge_sort(A, a = 0, b = None):
+    '''Sort A[a:b]'''
+    if b is None: b = len(A)
+    if 1 < b - a:
+        c = (a+b+1) // 2
+        merge_sort(A, a,c)
+        merge_sort(A, c,b)
+        L, R = A[a:c], A[c:b]
+        merge(L, R, A, len(L), len(R), a, b)
+
+
+def merge(L, R, A, i, j, a, b):
+    '''Merge sorted L[:i] and R[:j] into A[a:b]'''
+    if a < b:
+        if (j <= 0) or (i > 0 and L[i - 1] > R[j - 1]):
+            A[b-1] = L[i-1]
+            i = i - 1
+        else:
+            A[b-1] = R[j - 1]
+            j = j -1
+        merge(L, R, A, i, j, a, b-1)
