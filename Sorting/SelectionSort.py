c81b86b58b4dc6806ac69f0ebdda1e3b4564bff8
Vishal7017 <wavhalev@gmail.com>
2022-01-19 15:43:40 +0530

    Nato

--- Sorting/SelectionSort.py
+++ Sorting/SelectionSort.py
@@ -12,3 +12,12 @@ testList = [1,3,4,5,6,743,23,54,535,2355]
 print('')
 print(selection_sort(testList))
 print(testList)
+
+#Helper Function for selection sort
+def prefix_max(A, i):
+    '''Return index of maximum in A[:i + 1]'''
+    if i > 0:
+        j = prefix_max(A, i -1)
+        if A[i] < A[j]:
+            return j
+    return i
