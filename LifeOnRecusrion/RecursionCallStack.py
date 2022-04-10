3fabfa79b6027da8af19b7210b18c4d17fe329c9
Vishal7017 <wavhalev@gmail.com>
2022-04-10 20:03:48 +0530

    RecursionTricks

--- LifeOnRecusrion/RecursionCallStack.py
+++ LifeOnRecusrion/RecursionCallStack.py
@@ -1,12 +1,32 @@
-def sum_to_one(n):
-    result = 1
-    call_stack = []
-    while n != 1:
-        execution_context = {"n_value": n}
-        call_stack.append(execution_context)
-        n -= 1
-        print(call_stack)
-    print("BASE CASE RECAHED")
-    return result, call_stack
+# def sum_to_one(n):
+#     result = 1
+#     call_stack = []
+#     while n != 1:
+#         execution_context = {"n_value": n}
+#         call_stack.append(execution_context)
+#         n -= 1
+#         print(call_stack)
+#     print("BASE CASE RECAHED")
+#     return result, call_stack
 
-sum_to_one(10)
+# sum_to_one(10)
+
+
+def sum_to_one(n):
+  result = 1
+  call_stack = []
+  
+  while n != 1:
+    execution_context = {"n_value": n}
+    call_stack.append(execution_context)
+    n -= 1
+    print(call_stack)
+  print("BASE CASE REACHED")
+
+  while len(call_stack) != 0:
+    return_value = call_stack.pop()
+    print("Adding {0} to {1}".format(return_value["n_value"], result))
+    result += return_value["n_value"]
+  return result, call_stack
+
+sum_to_one(4)
