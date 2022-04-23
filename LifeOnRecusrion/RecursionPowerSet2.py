f4016f134798ff6caace91b797dceea338a4e062
Vishal7017 <wavhalev@gmail.com>
2022-04-23 16:02:40 +0530

    sometime

---
+++ LifeOnRecusrion/RecursionPowerSet2.py
@@ -0,0 +1,10 @@
+def power_set(my_list):
+    if len(my_list) == 0:
+        return [[]]
+    power_set_without_first = power_set(my_list[1:])
+    with_first = [ [my_list[0]] + rest for rest in power_set_without_first ]
+    return with_first + power_set_without_first
+
+print(power_set(['a', 'b', 'c']))
+
+
