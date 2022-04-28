e0053fd00432d0b88d8cd61b4ae1b7b48b894200
Vishal7017 <wavhalev@gmail.com>
2022-04-28 16:04:55 +0530

    asdf

--- IterationOverRecursion/IterationMinInList.py
+++ IterationOverRecursion/IterationMinInList.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+from xml.etree.ElementTree import ElementTree
+
+
 def find_min(my_list):
 	min = None
 	for element in my_list:
@@ -7,6 +10,20 @@ def find_min(my_list):
 			min = element
 	return min
 
+
+
 print(find_min([42,17,2,-1,67]))
 print(find_min([]))
 print(find_min([13,72,19,5,86]))
+
+
+def find_min2(my_list):
+	min = None
+	for element in my_list:
+		if not min or (element < min):
+			min = element
+	return min
+
+print(find_min2([42,17,2,-1,67]))
+print(find_min2([]))
+print(find_min2([13,72,19,5,86]))
