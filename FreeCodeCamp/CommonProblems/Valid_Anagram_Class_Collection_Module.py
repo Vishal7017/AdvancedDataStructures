55db7e7ccc61acc34eaf023b8d2720e612180bca
Vishal7017 <wavhalev@gmail.com>
2022-05-11 16:09:43 +0530

    Coding

---
+++ FreeCodeCamp/CommonProblems/Valid_Anagram_Class_Collection_Module.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+def are_anagrams(s1, s2):
+    if len(s1) != len(s2):
+        return False
+    return Counter(s1) == Counter(s2)
+
+
+def are_anagrams(s1, s2):
+    if len(s2) != len(s2):
+        return False
+    return sorted(s1) == sorted(s2)
--- LifeOnRecusrion/RecursionMinInList.py
+++ LifeOnRecusrion/RecursionMinInList.py
@@ -9,3 +9,11 @@ def find_min(my_list, min=None):
 
 print(find_min([3,43,-2,4,3]) == -2)
 print(find_min([100,22,12,12,324,12]) == 100)
+
+def find_min2(my_list, min=None):
+    if not my_list:
+        return min
+    
+    if not min or my_list[0] < min:
+        min = my_list[0]
+    return find_min(my_list[1:], min)
